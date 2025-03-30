d1e472e317e51fd7a01d29862587fe63506e4abf
Ccindy0171 <cindy.w0135@gmail.com>
2025-03-30 15:54:55 +0800

    misc: place service node

--- src/behavior_tree/behavior_tree/messages.py
+++ src/behavior_tree/behavior_tree/messages.py
@@ -4,7 +4,7 @@ from tk_nav_interfaces.srv import Goto, GotoGrasp, RelToAbs
 # from tinker_decision_msgs.srv import Goto, GotoGrasp, RelToAbs
 # from tinker_decision_msgs.srv import Drop
 # from tinker_decision_msgs.srv import Grasp
-from tinker_arm_msgs.srv import Grasp, Drop
+from tinker_arm_msgs.srv import Grasp, Drop, Place
 # from tinker_decision_msgs.srv import Announce, WaitForStart
 from tinker_audio_msgs.srv import TextToSpeech, WaitForStart, PhraseExtraction, GetConfirmation
 
