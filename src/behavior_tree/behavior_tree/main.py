41e4beefc45bea88243f4d32e59da27bfaf34d5e
Ccindy0171 <cindy.w0135@gmail.com>
2025-05-01 17:44:53 +0800

    misc

--- src/behavior_tree/behavior_tree/main.py
+++ src/behavior_tree/behavior_tree/main.py
@@ -2,7 +2,7 @@ import py_trees
 import py_trees_ros
 import rclpy
 
-# from .HelpMeCarry.Track import createFollowPerson, createFollowPersonAudio, createRepeatTrack
+from .HelpMeCarry.Track import createFollowPerson
 from .Receptionist.receptionist import createReceptionist
 from .grasp_intel_demo.grasp_intel import create_demo
 from .grasp_intel_demo.grasp_audio import createGraspAudio
@@ -158,33 +158,33 @@ def receptionist():
         tree.shutdown()
         rclpy.try_shutdown()
 
-# def test_follow():
-#     rclpy.init(args=None)
+def test_follow():
+    rclpy.init(args=None)
 
-#     root = createFollowPerson()
+    root = createFollowPerson()
 
-#     # make it a ros tree
-#     tree = py_trees_ros.trees.BehaviourTree(root)
-#     tree.setup(node_name="root_node", timeout=15)
+    # make it a ros tree
+    tree = py_trees_ros.trees.BehaviourTree(root)
+    tree.setup(node_name="root_node", timeout=15)
 
-#     # function for display the tree to standard output
-#     def print_tree(tree):
-#         print(py_trees.display.unicode_tree(root=tree.root, show_status=True))
-#         if PRINT_BLACKBOARD:
-#             print(py_trees.display.unicode_blackboard())
+    # function for display the tree to standard output
+    def print_tree(tree):
+        print(py_trees.display.unicode_tree(root=tree.root, show_status=True))
+        if PRINT_BLACKBOARD:
+            print(py_trees.display.unicode_blackboard())
 
-#     if PRINT_DEBUG:
-#         py_trees.logging.level = py_trees.logging.Level.DEBUG
+    if PRINT_DEBUG:
+        py_trees.logging.level = py_trees.logging.Level.DEBUG
     
-#     tree.tick_tock(period_ms=500.0,post_tick_handler=print_tree)
+    tree.tick_tock(period_ms=500.0,post_tick_handler=print_tree)
 
-#     try:
-#         rclpy.spin(tree.node)
-#     except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
-#         pass
-#     finally:
-#         tree.shutdown()
-#         rclpy.try_shutdown()
+    try:
+        rclpy.spin(tree.node)
+    except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
+        pass
+    finally:
+        tree.shutdown()
+        rclpy.try_shutdown()
 
 # def test_follow_audio():
 #     rclpy.init(args=None)
