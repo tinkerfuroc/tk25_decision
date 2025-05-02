7e5da170831e9383b78de949411b2ceeb5b37be7
Ccindy0171 <cindy.w0135@gmail.com>
2025-05-03 00:30:58 +0800

    misc

--- src/behavior_tree/behavior_tree/StoringGroceries/storing_groceries.py
+++ src/behavior_tree/behavior_tree/StoringGroceries/storing_groceries.py
@@ -70,7 +70,8 @@ POINT_PLACE = PointStamped(header=Header(stamp=rclpy.time.Time().to_msg(), frame
 
 ARM_POS_NAVIGATING = [x / 180 * math.pi for x in [-87.0, -40.0, 28.0, 0.0, 30.0, -86.0, 0.0]]
 ARM_POS_SCAN_MIDDLE = [x / 180 * math.pi for x in [-87.0, -40.0, 28.0, 60.0, 30.0, -86.0, 0.0]]
-ARM_POS_SCAN = [x / 180 * math.pi for x in [0.0, -50.0, 0.0, 66.0, 0.0, 55.0, 0.0]]
+ARM_POS_SCAN = [x / 180 * math.pi for x in [0.0, -43.0, 0.0, 45.0, 0.0, 31.0, 0.0]]
+# ARM_POS_SCAN = [x / 180 * math.pi for x in [0.0, -50.0, 0.0, 66.0, 0.0, 55.0, 0.0]]
 ARM_POS_PLACING = ARM_POS_NAVIGATING
 # ARM_POS_PLACING = [x / 180 * math.pi for x in [-87.6, -18.0, 8.3, 42.4, 1.6, -56.1, -20]]
 GRASP_POSE_DUMMY = Pose(position=Point(x=15.226666017642406, y=-0.4662523345900057, z=0.0),
