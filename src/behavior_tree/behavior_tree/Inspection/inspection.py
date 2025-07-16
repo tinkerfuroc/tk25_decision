1c7a1efaffb3e440378fb83a64f483027b526b26
Ccindy0171 <cindy.w0135@gmail.com>
2025-07-16 11:16:24 +0800

    update: better inspection

--- src/behavior_tree/behavior_tree/Inspection/inspection.py
+++ src/behavior_tree/behavior_tree/Inspection/inspection.py
@@ -2,7 +2,7 @@ import py_trees
 
 from behavior_tree.TemplateNodes.BaseBehaviors import BtNode_WriteToBlackboard
 from behavior_tree.TemplateNodes.Navigation import BtNode_GotoAction
-from behavior_tree.TemplateNodes.Audio import BtNode_Announce
+from behavior_tree.TemplateNodes.Audio import BtNode_Announce, BtNode_Listen
 
 from .customNodes import BtNode_PressEnterToSucceed
 
@@ -15,16 +15,6 @@ import math
 import json
 import select
 import sys
-
-# POINT_TO_PERSON = False
-TURN_PAN_TILT = True
-
-MAX_SCAN_DISTANCE = 4.5
-
-DEBUG_NO_GOTO = False
-
-DISABLE_FEATURE_MATCH = False
-DISABLE_FOLLOW_HEAD = False
 
 # read from `constant.json` in the same directory
 # load file
@@ -51,12 +41,15 @@ pose_exit = PoseStamped(header=Header(stamp=rclpy.time.Time().to_msg(), frame_id
                                                             w=constants["pose_exit"]["orientation"]["w"]))
                             )
 
+tinker_description = constants["tinker_description"]
+
 ARM_POS_NAVIGATING = [x / 180 * math.pi for x in constants["arm_pos_navigating"]]
 
 
 
 KEY_INSPECTION_POSE = "inspection_pose"
 KEY_EXIT_POSE = "exit_pose"
+KEY_LISTEN_RESULT = "listen_result"
 
 arm_service_name = "arm_joint_service"
 
@@ -77,12 +70,30 @@ def createToExit():
     root.add_child(py_trees.decorators.Retry(name="retry", child=BtNode_GotoAction("Go to exit", KEY_EXIT_POSE), num_failures=10))
     return root
 
+def createQandA():
+    # TODO (for audio)
+    # audio package should create a new node for general Q & A (just answering questions)
+    # the node should accept a question (string) and an optional background_info (string)
+    root = py_trees.composites.Sequence(name="Q & A", memory=True)
+    root.add_child(BtNode_Listen(name="listen", bb_dest_key=KEY_LISTEN_RESULT))
+    # TODO: replace these announce nodes with a call to the audio node for Q & A (listen result as question, tinker_description as background_info)
+    # announce the results with the call to audio instead
+    root.add_child(BtNode_Announce(name="announce I heard you", bb_source=None, message="I hear you, you said"))
+    root.add_child(BtNode_Announce(name="repeat result of listen", bb_source=KEY_LISTEN_RESULT))
+    return root
+
 def createReceptionist():
     root = py_trees.composites.Sequence(name="Inspection Root", memory=True)
     # write all the constants to blackboard first
     root.add_child(createConstantWriter())
     root.add_child(createToIspection)
-    root.add_child(BtNode_Announce("Announce: I am ready to inspect", "I am ready to inspect"))
-    root.add_child(BtNode_PressEnterToSucceed(name="Press Enter to start inspection"))
+
+    root.add_child(BtNode_Announce(name="Announce: I am ready to inspect", bb_source=None, message="I am Tinker, I am ready for inspection. I will briefly introduce myself."))
+    root.add_child(BtNode_Announce(name="inrtoduce self", bb_source=None, message=tinker_description))
+    
+    # answer three questions (currently just repeats the question without answering)
+    root.add_child(py_trees.decorators.Repeat(name="repeat 3 times", child=createQandA(), num_success=3))
+    
+    root.add_child(BtNode_Announce(name="announce leaving", bb_source=None, message="Heading to the exit."))
     root.add_child(createToExit)
     return root
