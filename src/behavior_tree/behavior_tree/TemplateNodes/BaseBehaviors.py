7d108f8052286c80a5bef10a2e2e12ec9239532c
Ccindy0171 <cindy.w0135@gmail.com>
2026-01-17 18:23:42 +0800

    feat: added wait keyboard press

--- src/behavior_tree/behavior_tree/TemplateNodes/BaseBehaviors.py
+++ src/behavior_tree/behavior_tree/TemplateNodes/BaseBehaviors.py
@@ -212,3 +212,43 @@ class BtNode_WaitTicks(py_trees.behaviour.Behaviour):
             return py_trees.common.Status.SUCCESS
         else:
             return py_trees.common.Status.RUNNING
+        
+class BtNode_WaitKeyboardPress(py_trees.behaviour.Behaviour):
+    def __init__(
+            self,
+            name: str,
+            key: str = None
+    ):
+        super().__init__(name)
+        self.key = key
+        self.pressed = False
+    
+    def initialise(self) -> None:
+        self.pressed = False
+        if self.key:
+            print(f"Press '{self.key}' to continue...")
+        else:
+            print('Press any key to continue...')
+
+    def update(self) -> Status:
+        import sys
+        import select
+        import termios
+        import tty
+
+        old_settings = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setcbreak(sys.stdin.fileno())
+            if select.select([sys.stdin], [], [], 0)[0]:
+                ch = sys.stdin.read(1)
+                if self.key and ch == self.key:
+                    self.pressed = True
+                elif ch and not self.key:
+                    self.pressed = True
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+        if self.pressed:
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.RUNNING
